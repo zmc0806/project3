7000b528bfe8c948a3e1b7ce82591a54ff90ef9c
zmc0806 <129798536+zmc0806@users.noreply.github.com>
2024-02-18 01:45:25 -0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
   let pathGenerator;
   let colorScale;
   let tooltip = { x: 0, y: 0, text: '', visible: false };
-  let maxMortality = 109;
+  let maxMortality = 102.75;
   let legendValues = [];
   let hoveredMortalityRate = null;
   let hoveredCountryMortality = null;
