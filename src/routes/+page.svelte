9f38f23b9aeb63b95188f09913bfb371303c03d6
zmc0806 <129798536+zmc0806@users.noreply.github.com>
2024-02-18 01:21:35 -0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -117,12 +117,12 @@ function resetLegendHighlight() {
       Respiratory death rate from seasonal influenza, age 65+, 2011
     </text>
 
-  <g class="legend" transform="translate({(width - legendValues.length * 40) / 2}, {height - 60})">
-    {#each legendValues as value, index}
-    <rect
-      x={index * (300 / legendValues.length)} // Adjust the width based on number of legend values
+    <g class="legend" transform="translate({(width - legendValues.length * 40) / 2}, {height - 60})">
+      {#each legendValues as value, index}
+      <rect
+      x={index * 40 - 80}
       y={0}
-      width={300 / legendValues.length} // Adjust the width based on number of legend values
+      width={300}
       height={50}
       fill="{colorScale(value)}"
       stroke="{hoveredCountryMortality !== null && hoveredCountryMortality >= value && hoveredCountryMortality < value + maxMortality / 9 ? 'black' : 'none'}"
@@ -197,8 +197,6 @@ function resetLegendHighlight() {
     text-anchor: start;
   } 
 </style>
-
-
 
 
 
