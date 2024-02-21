043ac9fc21ce4a60f43fb5cea68b2e0c08a3382a
YulinChen12 <yuc093@ucsd.edu>
2024-02-20 20:23:14 -0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -86,7 +86,7 @@ Our visualization strives to provide an accessible and informative view of seaso
     hoveredMortalityRate = null;
   }
   
-  const width = 1500;
+  const width = 1600;
   const height = 760;
   let selectedCountry = null;
 
